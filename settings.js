1849887e55adb21060ccbac036eefa75dcd96865
Drenx12 <makushadennis54@gmail.com>
2025-05-07 12:39:36 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,13 +3,13 @@
 const chalk = require("chalk")
 const fs = require("fs")
 
-global.ownerNumber = ["263786115435@s.whatsapp.net"]
+global.ownerNumber = ["263789085533@s.whatsapp.net"]
 global.botnumber = "263786115435"
 global.autotyping = false;
 global.autorecording = false;
 global.botnumber = ['263786115435']
-global.DevName = "iconic tech"
-global.ownerName = "iconic tech"
+global.DevName = "𝐷𝐵𝑌☆"
+global.ownerName = "𝐷𝐵𝑌☆"
 global.Botname = "Terminator QR MD"
 global.versionBot = "𝟏"
 global.packname = "iconic tech"
